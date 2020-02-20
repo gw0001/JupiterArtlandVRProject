5eeea67feba0581b4c22d9324a0550d425e633d2
Fletcher <42897038+Fletcher40340889@users.noreply.github.com>
2020-02-20 21:28:19 +0000

    NextExhibit Script added
    NextExhibit script added, inital comments added (Author, dates etc)

---
+++ Assets/Scripts/NextExhibit.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * JUPITER ARTLAND VR EXPERIENCE 
+ *
+ * Author(s): Fletcher Moore
+ * Date: 20/02/2020
+ * Date last modified: 
+ */
+
+
+ //Libraries 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * NextExhibit.cs 
+ *
+ * NextExhibit
+ *
+ * Script allows Navigation from Cells of Life to 
+ * Separation In The Evening
+ */
+public class NextExhibit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
