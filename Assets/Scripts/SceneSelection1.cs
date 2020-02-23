8d62293880da03c1b207507929345fdf0a08e978
Fletcher <42897038+Fletcher40340889@users.noreply.github.com>
2020-02-23 20:41:46 +0000

    SceneSelection1 script edit
    Script for SceneSelection1 script edited and "Teleport" tag added

--- Assets/Scripts/SceneSelection1.cs
+++ Assets/Scripts/SceneSelection1.cs
@@ -10,6 +10,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * SceneSelection1.cs
@@ -24,15 +25,60 @@ using UnityEngine;
  */
 public class SceneSelection1 : MonoBehaviour
 {
+    //Variables
+    //Image
+    public Image timerSphere;
+    //Total time for timer
+    public float totalTime = 2;
+    //GVR status
+    public bool status;
+    //GVR timer
+    public float timer;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /*
+    * UPDATE METHOD
+    *
+    * 
+    */
     void Update()
     {
-        
+        if (status)
+        {
+            //sets time 
+            timer += Time.deltaTime;
+            //fills timerSphere depending on timer
+            timerSphere.fillAmount = timer / totalTime;
+        }
+    }
+
+    /*
+     * GVRON METHOD
+     *
+     * Activates the Timer
+     */
+    public void GVROn()
+    {
+        status = true;
+    }
+
+    /*
+    * GVROFF METHOD
+    *
+    * Deactivates the Timer
+    */
+    public void GVROff()
+    {
+        status = false;
+        //Retruns timer to 0
+        timer = 0;
+        //returns the fill of the sphere to 0 
+        timerSphere.fillAmount = 0;
     }
 }
