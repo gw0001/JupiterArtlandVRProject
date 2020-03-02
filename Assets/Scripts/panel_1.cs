04e3bcd1c4a60265e0a6c88028ae512098b354b8
GregorKelly <33128671+GregorKelly@users.noreply.github.com>
2020-03-02 11:02:59 +0000

    Added working Tutuorial

---
+++ Assets/Scripts/panel_1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_1 : MonoBehaviour
+{
+    private Canvas CanvasObject; // Assign in inspector
+    private bool PageUsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasObject = GetComponent<Canvas>();
+        PageUsed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && PageUsed == false)
+        {
+            CanvasObject.enabled = !CanvasObject.enabled;
+            PageUsed = true;
+        }
+    }
+}
---
+++ Assets/Scripts/panel_2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_2 : MonoBehaviour
+{
+    private Canvas CanvasObject;
+    private bool PageUsed;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasObject = GetComponent<Canvas>();
+        CanvasObject.enabled = !CanvasObject.enabled;
+        PageUsed = false;
+        count = 0;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && PageUsed == true && count<2)
+        {
+            CanvasObject.enabled = !CanvasObject.enabled;
+            count = count + 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && PageUsed == false)
+        {
+            count = count + 1;
+            CanvasObject.enabled = !CanvasObject.enabled;
+            PageUsed = true;
+        }        
+    }
+}
