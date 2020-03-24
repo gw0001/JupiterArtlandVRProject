2ae15677387a6a821e38b586e3d100a29d3ce6b8
HighlanderJeff <51862028+HighlanderJeff@users.noreply.github.com>
2020-03-24 21:39:52 +0000

    gaze script iissue on cells*13
    All of the other features work elsewhere as before but this version has an edited scene.
    
    The script works on two buttons instead of one, I did try to configure it to just one button but will work on it.

---
+++ Assets/Scripts/ToogleHub.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ToogleHub : MonoBehaviour
+{
+    private string messageText="If you are reading this... good";
+    public GameObject MakeVisable, makeInvisable;
+
+    private void Start()
+    {
+        MakeVisable.GetComponent<GameObject>();
+        makeInvisable.GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        bool o = Input.GetKeyDown(KeyCode.O);
+        bool p = Input.GetKeyDown(KeyCode.P);
+        // User presses 1
+        if (o)
+        {
+            makeInvisable.SetActive(false);
+            MakeVisable.SetActive(true);
+            Debug.Log(messageText);
+        }
+        // User presses 1
+        if (p)
+        {
+            makeInvisable.SetActive(true);
+            MakeVisable.SetActive(false);
+            Debug.Log(messageText);
+        }
+    }
+}
